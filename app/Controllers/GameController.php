1e6f1eeb901272f686b1bcd4e516788ec6dc1653
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-11-20 16:30:32 +0100

    Suppression de la classe Card et ajout de la structure de GameController

---
+++ app/Controllers/GameController.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Models\Card;
+use Core\BaseController;
+
+class GameController extends BaseController
--- app/Models/Card.php
+++ app/Models/Card.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Models;
+
 class Card
 {
     private $id;
