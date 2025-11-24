13342045bcc2b81ce48ade069c94a919edaba4d6
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-11-24 15:48:49 +0100

    Ajout de la classe User pour gérer la création et la recherche d'utilisateurs en base de données.

---
+++ app/Models/User.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Core\Database;
+
+class User
+{
+    /**
+     * 
+     * Crée un nouvel utilisateur en base de données
+     */
+    public function create($login, $password, $email, $nom, $prenom)
+    {
+        $pdo = Database::getPdo();
+
+        $sql = "INSERT INTO utilisateurs(login, mot_de_passe, email, nom, prenom, date_creation)
+            VALUES (?, ?, ?, ?, ?? NOW())";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$login, $password, $email, $nom, $prenom]);
+        return $stmt;
+    }
+
+    /**
+     * Cherhe un utilisateur par son login 
+     */
+    public function findByLogin($login)
+    {
+        $pdo = Database::getPdo();
+
+        $sql = "SELECT * FROM utilisateurs WHERE login = ?";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$login]);
+
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+    /**
+     * Vérifie si un login existe déja 
+     */
+    public function exists($login)
+    {
+        $user = $this->findByLogin($login);
+        return $user !== false;
+    }
+}
