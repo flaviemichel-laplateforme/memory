23c9611160406ab2c6872468326dc0bf402003d9
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-11-25 11:46:13 +0100

    Ajout de la méthode de mise à jour des informations utilisateur et correction de la requête SQL pour la mise à jour sans mot de passe.

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,49 @@ class User
         $user = $this->findByLogin($login);
         return $user !== false;
     }
+
+    /**
+     * Met à jour les informations de l'utilisateur
+     */
+    public function update($login, $email, $nom, $prenom, $password = null)
+    {
+        if (!isset($_SESSION['user']['id'])) {
+            return false;
+        }
+
+        $id = $_SESSION['user']['id'];
+        $pdo = Database::getPdo();
+
+        if ($password) {
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+            $sql = "UPDATE utilisateurs
+                    SET login = ?, email = ?, nom = ?, prenom = ?, password = ?
+                    WHERE id = ?";
+
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$login, $email, $nom, $prenom, $hashedPassword, $id]);
+        } else {
+
+            $sql = "UPDATE utilisateurs
+                    SET login = ?, email = ?, nom = ?, prenom = ?, 
+                    WHERE id = ?";
+
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$login, $email, $nom, $prenom, $id]);
+        }
+        return true;
+    }
+
+    /**
+     * Recupère toutes les infos
+     * Utile pour pré-remplir le formulaire
+     */
+    public function findById($id)
+    {
+        $pdo = Database::getPdo();
+        $stmt = $pdo->prepare("SELECT * FROM utilisateurs WHERE id = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch();
+    }
 }
